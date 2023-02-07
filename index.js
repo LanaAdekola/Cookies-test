c19ed4639f212e8c825e7e90a7fff6b63b09dc0c
LanaAdekola <adekolaadelana@gmail.com>
2023-02-07 10:42:40 +0100

    x

--- index.js
+++ index.js
@@ -1,166 +1,50 @@
-*{
-    box-sizing: border-box;
-}
+const modal = document.getElementById('modal')
+const modalCloseBtn = document.getElementById('modal-close-btn')
+const consentForm = document.getElementById('consent-form')
+const modalText = document.getElementById('modal-text')
+const declineBtn = document.getElementById('decline-btn')
+const modalChoiceBtns = document.getElementById('modal-choice-btns')
 
-html, body {
-    margin: 0;
-    padding: 0;
-    font-family: 'Roboto';
-    color: #222;
-}
+setTimeout(function(){
+    modal.style.display = 'inline'
+}, 1500)
 
-/* typography */
+modalCloseBtn.addEventListener('click', function(){
+    modal.style.display = 'none'
+})
 
-h1{
-    font-size: 48px;
-    line-height: 60px;
-    font-weight: 900;
-}
+declineBtn.addEventListener('mouseenter', function(){
+    modalChoiceBtns.classList.toggle('modal-choice-btns-reverse')
+})
 
-h2{
-    margin-top: 0;
-}
 
-h3{
-    font-size: 20px;
-    font-weight: 700; 
-    line-height: 30px;
-}
-
-p{
-    font-size: 18px;
-    line-height: 28px;
-
-}
-
-ul{
-    list-style: none;
-    padding: 5px 0 0 0;
-}
-
-li{
-    padding: 15px;
-    border-radius: 5px;
-    margin-bottom: 12px;
-    font-weight: 500;
-    font-size: 22px;
-}
-
-.li-green{background-color:#E5FDEA}
-.li-yellow{background-color:#FCFDD6}
-.li-blue{background-color:#E5F9FE}
-.li-pink{background-color:#FCF1FE}
-
-/* layout */
-
-img{
-    width: 100%;
-}
-
-main{
-    width: 400px;
-    margin: 0 auto;
-}
-
-/* modal */
+consentForm.addEventListener('submit', function(e){
+    e.preventDefault()
+    
+    const consentFormData = new FormData(consentForm)
+    const fullName = consentFormData.get('fullName')
+    
+    modalText.innerHTML = `
+    <div class="modal-inner-loading">
+        <img src="images/loading.svg" class="loading">
+        <p id="upload-text">Uploading your data to the dark web...</p>
+    </div>` 
+    
+    setTimeout(function(){
+        document.getElementById('upload-text').innerText = `
+        Making the sale...`
+    }, 1500)
+    
+    
+    setTimeout(function(){
+        document.getElementById('modal-inner').innerHTML = `
+        <h2>Thanks <span class="modal-display-name">${fullName}</span>, you sucker! </h2>
+        <p>We just sold the rights to your eternal soul.</p>
+        <div class="idiot-gif">
+            <img src="images/pirate.gif">
+        </div>
+    `
+    modalCloseBtn.disabled = false
+    }, 3000)
   
-.modal {
-    display: none;
-    position: fixed;
-    right: 0;
-    left: 0;
-    top: 0;
-    bottom: 0;
-    margin: auto;
-    height: 420px;
-    width: 350px;
-    border-radius: 5px;
-    box-shadow: 0px 0px 6px 2px #666;
-    background-color: #212529;
-    color: whitesmoke;
-}
-
-.modal-inner{
-    width: 290px;
-    margin: 0 auto;
-}
-
-.modal-inner-loading{
-    text-align: center;
-}
-
-/* modal buttons */
-
-.close-modal-btn-container{
-    text-align: right;
-    margin: 9px;
-}
-
-.modal-close-btn {
-  font-size: 22px;
-  font-weight: bold;
-  background-color: transparent;
-  border: 1px solid transparent;
-  color: whitesmoke;
-}
-
-.modal-close-btn:hover,
-.modal-close-btn:active{
-    color: red;
-    cursor: pointer;
-}
-
-.modal-close-btn:disabled{
-    color: whitesmoke;
-    opacity: 0.2;
-    cursor: not-allowed;
-}
-
-.modal-choice-btns{
-    margin-top: 10px;
-    padding: 8px 16px;
-    display: flex;
-    justify-content: center;
-}
-
-.modal-btn{
-    padding: 10px 24px;
-    cursor: pointer;
-}
-
-/* modal input */
-
-input{
-    margin-top: 10px; 
-    border: 1px solid #212529;
-    padding: 10px;
-    width: 100%;
-    border-radius: 3px;
-}
-
-/* modal after submit */
-
-.loading{
-    width: 100%; 
-    margin-top: 10px;
-}
-
-.modal-display-name{
-    color: fuchsia;
-}
-
-.modal-text{
-    height: 140px;
-    line-height: 23px;
-    margin-bottom: 0;
-}
-
-.idiot-gif{
-    width: 95%;
-    margin: 0 auto;
-}
-
-.idiot-gif img{
-    box-shadow: 1px 1px 3px #999;
-    border-radius: 5px;
-}
+}) 
